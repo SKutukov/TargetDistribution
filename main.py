5346acba6ef7f346adcf9fb3f20285c3d6457921
SKutukov <skutukov96@gmail.com>
2017-11-04 02:31:22 +0300

    add task generator

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 import numpy as np
-maxD=20
-n=4
+import random
+
+task_number = 20
+min_guns_amount=20
+max_guns_amount=100
+min_targets_amount=4
+max_targets_amount=10
+min_cost=5
+max_cost=50
+min_weight=4
+max_weight=10
+def generate_tasks(task_number,min_ga,max_ga,min_ta,max_ta,min_c,max_c,min_w,max_w):
+    for i in range(0,task_number):
+        m=random.randint(min_ga,max_ga)
+        n = random.randint(min_ta, max_ta)
+        C=[]
+        D=[]
+        for j in range (0,n):
+            C.append(random.randint(min_c,max_c))
+            D.append(random.randint(min_w, max_w))
+        print ("Task ", i, ":")
+        print(m,' ', n)
+        print(C)
+        print(D)
+        print("end")
+
+generate_tasks(task_number,min_guns_amount,max_guns_amount,min_targets_amount,max_targets_amount,min_cost,max_cost,min_weight,max_weight)
+
+maxD = 20
+n = 4
 C = np.array([40, 45, 32, 14])
 D = np.array([5, 9, 8, 7])
 F=np.zeros((4,20))
@@ -46,5 +74,5 @@ def knapsack_dylp(A,B,C):
 
 Result,Cost=knapsack_dylp(D,maxD,C)
 
-print(Result)
-print(Cost)
+#print(Result)
+#print(Cost)
